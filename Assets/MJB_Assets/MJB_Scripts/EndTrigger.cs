a9d8554e59dd90e0045372b936738f4344757ae5
xalswox <qoalswo123@icloud.com>
2023-06-19 15:32:29 +0900

    Fix: End Trigger 플레이어만 충돌 Trigger 발생 하도록 수정

--- Assets/MJB_Assets/MJB_Scripts/EndTrigger.cs
+++ Assets/MJB_Assets/MJB_Scripts/EndTrigger.cs
@@ -9,8 +9,13 @@ public class EndTrigger : MonoBehaviour
     // 호출 되었을 때 게임 완료를 발생시킨다.
     private void OnTriggerEnter(Collider other)
     {
-        GameManager.instance.CompleteGame();
-        // 게임 오브젝트 플레이어를 삭제한다.
-        Destroy(other.gameObject);
+        // 태그가 플레이어일 때
+        if (other.gameObject.tag == "Player")
+        {
+            GameManager.instance.CompleteGame();
+            // 게임 오브젝트 플레이어를 삭제한다.
+            Destroy(other.gameObject);
+
+        }
     }
 }
